3e98a837308d8c696c9ee1e423232c9a220e2521
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-10-08 00:09:36 -0400

    git init

---
+++ script/app.js
@@ -0,0 +1,141 @@
+
+"use strict";
+
+((core) =>
+{
+    /**
+     * Inject the Navigation bar into the Header element and highlight the active link based on the pageName parameter
+     *
+     * @param {string} pageName
+     */
+    function loadHeader(pageName)
+    {
+      // inject the Header
+      $.get("./Views/components/header.html", function(data)
+      {
+        $("header").html(data); // load the navigation bar
+
+
+
+        $(`#${pageName}`).addClass("active"); // highlight active link
+
+        // loop through each anchor tag in the unordered list and 
+        // add an event listener / handler to allow for 
+        // content injection
+        $("a").on("click", function()
+        { 
+          $(`#${router.ActiveLink}`).removeClass("active"); // removes highlighted link
+          router.ActiveLink = $(this).attr("id");
+          loadContent(router.ActiveLink, ActiveLinkCallBack(router.ActiveLink));
+          $(`#${router.ActiveLink}`).addClass("active"); // applies highlighted link to new page
+          history.pushState({},"", router.ActiveLink); // this replaces the url displayed in the browser
+        });
+
+        // make it look like each nav item is an active link
+        $("a").on("mouseover", function()
+        {
+          $(this).css('cursor', 'pointer');
+        });
+      });
+    }
+
+    /**
+     * Inject page content in the main element 
+     *
+     * @param {string} pageName
+     * @param {function} callback
+     * @returns {void}
+     */
+    function loadContent(pageName, callback)
+    {
+      // inject content
+      $.get(`./Views/content/${pageName}.html`, function(data)
+      {
+        $("main").html(data);
+
+        callback();
+      });
+      
+    }
+
+    function loadFooter()
+    {
+      // inject the Footer
+      $.get("./Views/components/footer.html", function(data)
+      {
+        $("footer").html(data);
+      });
+    }
+
+    function displayHome()
+    {
+        
+    }
+
+    function displayCoursework()
+    {
+
+    }
+
+    function displayLabs()
+    {
+
+    }
+
+    function displayProject()
+    {
+
+    }
+
+     function displayLab1()
+    {
+
+    } 
+  
+
+     function displayLab2()
+    {
+
+    }
+    
+
+    function display404()
+    {
+
+    }
+
+    function ActiveLinkCallBack(activeLink)
+    {
+      switch (activeLink) 
+      {
+        case "home": return displayHome;
+        case "coursework": return displayCoursework;
+        case "Labs": return displayLabs;
+        case "project": return displayProject;
+        case "lab1": return displayLab1;
+        case "lab2": return displayLab2;
+        case "404": return display404;
+        default:
+          console.error("ERROR: callback does not exist: " + activeLink);
+          break;
+      }
+    }
+
+  
+
+    function Start()
+    {
+        console.log("App Started...");
+
+        loadHeader(router.ActiveLink);
+      
+        loadContent(router.ActiveLink, ActiveLinkCallBack(router.ActiveLink));
+
+        loadFooter();
+    }
+
+    window.addEventListener("load", Start);
+
+    core.Start = Start;
+
+})(core || (core={}));
